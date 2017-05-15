645c3cddd6cd0757c7f7ed7963b4ce0ff6d8665f
darksun <lujun9972@gmail.com>
2017-05-15 17:21:39 +0800

    update

--- word_grouping.py
+++ word_grouping.py
@@ -6,11 +6,12 @@ def distance(group, s):
     'Calulate the average distance between seq S and seqs in GROUP'
     if not group:
         return 0
-    distances = list(edit_distance(s, seq)[0] for seq in group)
+    distances = list(edit_distance(s, seq)[0] if seq[0:2] == s[0:2] else 9999
+                     for seq in group)
     return sum(distances) / len(distances)
 
 
-def grouping(groups, s):
+def grouping(groups, s, accuracy=2):
     'grouping seq S into one of group in GROUPS'
     if not groups:
         groups.append([])
@@ -18,7 +19,7 @@ def grouping(groups, s):
     min_distance = min(distances)
     min_pos = distances.index(min_distance)
     # print('min_distance=', min_distance, "min_pos=", min_pos)
-    if min_distance <= 2:
+    if min_distance <= accuracy:
         groups[min_pos].append(s)
     else:
         groups.append([s])
